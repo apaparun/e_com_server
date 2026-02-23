a18c0f3ca9a76f184898424ee7ee0c453121f3bc
arunprasanth_ap <arun.ap@praniontech.com>
2026-02-23 22:40:19 +0530

    ignored untracked files

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose')
 require('dotenv').config();
+
 const app = express();
+
+
+app.use(bodyParser.json());
+app.use(morgan('tiny'))
+app.use(cors())
+// app.options('*', cors());
 const hostname = process.env.APP_HOSTNAME;
 const port = process.env.PORT;
+
+mongoose.connect(process.env.MONGODB_URL)
 console.log("hostname", hostname);
 // const authorization = (_, res, next) => {
 //     const isAuth = false;
